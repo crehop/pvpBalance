d2e79a83f92200800300d7b04ada0d80403d53ec
chris <crehop2@gmail.com>
2013-08-16 21:31:00 -0700

    Revert "e"
    This reverts commit ccebb64b8a2bc4ec86e14ddafd69348a6085c011.

--- src/PvpBalance/DBZListener.java
+++ src/PvpBalance/DBZListener.java
@@ -2,8 +2,6 @@ package PvpBalance;
 
 import java.util.Random;
 
-import me.ThaH3lper.com.DungeonAPI;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -30,6 +28,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -66,7 +65,6 @@ public class DBZListener implements Listener
 		{
 			quitPlayer.setHealth(0f);
 		}
-<<<<<<< HEAD
 		if(pp.isInPVP())
 		{
 			pp.sethealth(0);
@@ -76,8 +74,6 @@ public class DBZListener implements Listener
 		{
 			pp.getParty().leave(pp);
 		}
-=======
->>>>>>> parent of 45fd277... Added Party System, Added Check On ItemBreak Update Health
 		PvpHandler.removePvpPlayer(pp);
 	}
 	
@@ -99,7 +95,7 @@ public class DBZListener implements Listener
 	{
 		//if(event.isCancelled())
 		//	return;
-		if(!DungeonAPI.canhit(event))
+		if(!Damage.partyCanHit(event.getEntity(), event.getDamager()))
 		{
 			event.setCancelled(true);
 			return;
@@ -536,6 +532,7 @@ public class DBZListener implements Listener
 	        PvpHandler.getPvpPlayer(player).setArmorEventLastTick(1);
 	    }
 	}
+	
 	@EventHandler
 	public void shotBow(EntityShootBowEvent event){
 		if(event.getEntity() instanceof Player){
@@ -545,6 +542,12 @@ public class DBZListener implements Listener
 				player.sendMessage(ChatColor.RED + "You must pull the bow all the way back to fire!");
 			}
 		}
+	}
+	
+	@EventHandler
+	public void onPlayerItemBreakEvent(PlayerItemBreakEvent event)
+	{
+		Damage.calcArmor(event.getPlayer());
 	}
 	
 	private boolean isArmor(ItemStack is)
